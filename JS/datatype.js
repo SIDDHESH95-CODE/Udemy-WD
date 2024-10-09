fb50a1b41a6c3db13fae153d57a9d24021fe05ee
Siddhesh Gaonkar <siddheshgaonkar95@gmail.com>
2024-10-09 20:03:52 +0530

    1st commit

---
+++ JS/datatype.js
@@ -0,0 +1,31 @@
+console.log("Datatypes in JS");
+
+/*
+
+String
+Number
+Boolean
+BigInt
+
+undefined
+null
+
+Object
+Array
+Functions
+
+Symbol
+
+*/
+
+let name = "Josh";
+let score = 100;
+let isAdded = true;
+let value = undefined;
+
+let obj = {
+    name: "Kash",
+    rank: 3
+}
+
+let arr = [1, 2, 3, 4, 5]
---
+++ JS/printing.js
@@ -0,0 +1,5 @@
+console.log("Hello");
+
+console.table({ Name: "Siddhesh", Marks: 87 });
+
+console.clear("hey")

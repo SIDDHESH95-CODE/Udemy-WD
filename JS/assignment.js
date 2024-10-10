c19342cacef70c2bd5a7eb5ff4a5ed7b13596977
Siddhesh Gaonkar <siddheshgaonkar95@gmail.com>
2024-10-10 09:36:46 +0530

    1st commit

---
+++ JS/assignment.js
@@ -0,0 +1,7 @@
+let num1 = 4;
+
+// num1 = num1 + 6
+num1 += 6;
+
+num1 -= 6;
+console.log(num1);
---
+++ JS/operations.js
@@ -0,0 +1,44 @@
+let addition = 55 + 44;
+
+
+let score = 104;
+let bonus = 55;
+
+let myScore = score + bonus;
+
+console.log(addition);
+addition++;
+console.log(addition);
+console.log(myScore);
+myScore--;
+console.log(myScore);
+
+let subtract = 33 - 44;
+let multi = 33 * 44;
+let divide = 33 / 44;
+let reminder = 33 % 44;
+let expo = 33 ** 44;
+
+
+// Comparison Oprators:
+let num1 = 3;
+let num2 = 3;
+let num3 = 6;
+
+console.log(num1 == num2);
+console.log(num1 == num3);
+console.log(num2 != num3);
+
+// && : AND
+// || : OR
+// ! : NOT or Reverse
+
+let isLoggedin = true;
+let isPaid = true;
+
+console.log(isLoggedin && isPaid);
+
+let isUser = true;
+let isStudent = false;
+
+console.log(isUser || isStudent);

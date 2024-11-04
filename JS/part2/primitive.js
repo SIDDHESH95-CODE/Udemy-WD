c935ef146b57a60a958a9c8dda996ca1f1bf8deb
Siddhesh Gaonkar <siddheshgaonkar95@gmail.com>
2024-11-04 18:11:09 +0530

    1st commit

--- JS/part2/primitive.js
+++ JS/part2/primitive.js
@@ -1,25 +1,25 @@
 // Number:
 let balance = 125;
 
-console.log(balance);
-console.log(typeof(balance));
+// console.log(balance);
+// console.log(typeof(balance));
 // console.log(typeof balance);
 
 let newBalance = new Number(125);
-console.log(newBalance);
+// console.log(newBalance);
 
 
 // Boolean:
 let isActive = new Boolean(false);
-console.log(isActive);
+// console.log(isActive);
 
 
 // null & undefined:
 let firstName = null;
 let lastName = undefined;
 
-console.log(firstName);
-console.log(lastName);
+// console.log(firstName);
+// console.log(lastName);
 
 
 //String:
@@ -28,4 +28,16 @@ let last = "Javier"
 
 // let fullName = first + " " + last
 let fullName = `${first} Peach`
-console.log(fullName);
+// console.log(fullName);
+
+
+let sm1 = Symbol("Siddhesh");
+// console.log(sm1);
+
+const data = {
+    firstName: "Siddhesh",
+    lastName: "Gaonkar"
+}
+
+console.log(data.firstName);
+console.log(data.lastName);

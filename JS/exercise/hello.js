94967cd45080402c13e85e493e5dace44309fa87
Siddhesh Gaonkar <siddheshgaonkar95@gmail.com>
2025-01-22 14:41:01 +0530

    1st commit

--- JS/exercise/hello.js
+++ JS/exercise/hello.js
@@ -117,10 +117,15 @@ let swap = document.querySelector(".swap");
 let h1 = document.querySelector("h1");
 let img1 = document.querySelector(".img1");
 let img2 = document.querySelector(".img2");
-let src1 = img1.src
-let src2 = img2.src
+// let src1 = img1.src;
+// let src2 = img2.src;
 let inputText = document.querySelector("#name");
 let formSubmit = document.querySelector("#submit");
+let ul = document.querySelector("ul");
+let addList = document.querySelector("#add");
+let removeList = document.querySelector("#add");
+let timerDisplay = document.querySelector("#timer");
+// console.log(addList);
 
 /* btn.addEventListener("click", () => {
     h1.textContent = "Greetings.."
@@ -132,17 +137,49 @@ let formSubmit = document.querySelector("#submit");
     img2.src = src1
     }) */
    
-   formSubmit.addEventListener("click", (evt) => {
+/*    formSubmit.addEventListener("click", (evt) => {
        evt.preventDefault();
        if (inputText.value === "") alert("Input is invalid");
        else console.log(inputText.value);
-})
+}) */
+
+/* addList.addEventListener("click", () => {
+    let li = document.createElement("li");
+    li.textContent = "Apple"
+    li.textContent = "Banana"
+    ul.appendChild(li);
+    console.log(li);
+}) */
 
 
+/* let a = 1;
+
+setInterval(() => {
+    timerDisplay.textContent = a;
+    console.log(a);
+    a++;
+}, 1000); */
+
+/* let btnHome = document.querySelector(".homeBtn");
+let btnAbout = document.querySelector(".aboutBtn");
+let homeSection = document.querySelector("#home");
+let aboutSection = document.querySelector(".about"); */
+
+/* btnHome.addEventListener("click", () => {
+    console.log("Home");
+    console.log(homeSection);
+    homeSection.style.display = "block";
+}) */
 
 
+// btnAbout.addEventListener("click", () => {
+//     console.log("about");
+// })
 
+/* window.addEventListener("scroll", () => {
 
+    const scrlY = window.scrollY;
 
-
+    console.log(scrlY);
+}) */
 

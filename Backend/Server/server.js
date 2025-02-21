d4f98e17bb2b4303db2a2caa29f715bfe581d8f3
Siddhesh Gaonkar <siddheshgaonkar95@gmail.com>
2025-02-21 08:53:51 +0530

    1st commit

---
+++ Backend/Server/server.js
@@ -0,0 +1,42 @@
+const http = require("http");
+const fs = require("fs");
+const path = require("path");
+
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+  const filePath = path.join(
+    __dirname,
+    req.url === "/" ? "index.html" : req.url
+  );
+
+  const extName = path.extname(filePath).toLocaleLowerCase();
+
+  let mimeTypes = {
+    ".html": "text/html",
+    ".css": "text/css",
+    ".js": "text/javascript",
+    ".png": "image/png",
+  };
+
+  const contentType = mimeTypes[extName] || "application/octet-stream";
+
+  fs.readFile(filePath, (err, content) => {
+    if (err) {
+      if (err.code === "ENOENT") {
+        res.writeHead(404, { "Content-Type": "text/html" });
+        res.end("404: File not found bro");
+      } else {
+        res.writeHead(500);
+        res.end(`Server Error: ${err.code}`);
+      }
+    } else {
+      res.writeHead(200, { "Content-Type": contentType });
+      res.end(content, "utf8");
+    }
+  });
+});
+
+server.listen(port, () => {
+  console.log(`Server is listening on port ${port}`);
+});

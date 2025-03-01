3d75ad3c576e69f3b139367a951f7c71b7cd84ea
Siddhesh Gaonkar <siddheshgaonkar95@gmail.com>
2025-03-01 11:00:19 +0530

    intermediate mongodb done

--- Backend/05_intermediate-mongoDB/mongodbBackendApp/routes/index.js
+++ Backend/05_intermediate-mongoDB/mongodbBackendApp/routes/index.js
@@ -18,10 +18,39 @@ router.get("/create", async (req, res) => {
   res.send(createUser);
 })
 
-router.get("/find", async (req, res) => {
+/* router.get("/find", async (req, res) => {
   let regexValue = new RegExp("^siddhant$", "i");
   const foundUser = await userModel.find({ username: regexValue })
   res.send(foundUser);
+}) */
+
+/* router.get("/finduser", async (req, res) => {
+  let user = await userModel.find({ categories: { $all: ["traveling"] } })
+  res.send(user);
+}) */
+
+/* router.get("/find", async (req, res) => {
+  let startDate = new Date("2025-02-28"); 
+  let endDate = new Date("2025-03-01"); 
+  const dataByDate = await userModel.find({dateCreated: {$gte: startDate, $lte: endDate}});
+  res.send(dataByDate);
+}) */
+
+/* router.get("/search", async (req, res) => {
+  let categoryAvailable = await userModel.find({ email: { $exists: true } });
+  res.send(categoryAvailable);
+}) */
+
+router.get("/searchbylen", async (req, res) => {
+  let userByLen = await userModel.find({
+    $expr: {
+      $and: [
+        { $gte: [{ $strLenCP: "$email" }, 0] },
+        { $lte: [{ $strLenCP: "$email" }, 17] }
+      ]
+    }
+  })
+  res.send(userByLen);
 })
 
 module.exports = router;
